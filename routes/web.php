2b22ecac99af79fa833f53331082a09154094e9b
pramodpandit72 <nwdpramod12305901@gmail.com>
2026-06-06 21:52:25 +0530

    Refactor routes in web.php to comment out unused routes and improve organization

--- routes/web.php
+++ routes/web.php
@@ -12,242 +12,257 @@ use App\Http\Controllers\DataRetrival;
 use App\Http\Controllers\BasicController;
 use App\Http\Controllers\QueryBuilder;
 
-Route::get('/insert', [QueryBuilder::class, 'insert']);
-Route::get('/show', [QueryBuilder::class, 'show']);
-Route::get('/update', [QueryBuilder::class, 'update']);
-Route::get('/delete', [QueryBuilder::class, 'delete']);
+// Route::get('/insert', [QueryBuilder::class, 'insert']);
+// Route::get('/show', [QueryBuilder::class, 'show']);
+// Route::get('/update', [QueryBuilder::class, 'update']);
+// Route::get('/delete', [QueryBuilder::class, 'delete']);
 
 
-// Route Middleware
-Route::get('/b', function() {
-    return ("Hello From Route middleware");
-})->middleware("ageCheck");
+// // Route Middleware
+// Route::get('/b', function() {
+//     return ("Hello From Route middleware");
+// })->middleware("ageCheck");
 
-// Group Middleware
-Route::middleware('ageGroup')->group(function() {
-    Route::get('/one', function() {
-        return "This is first Group Middleware";
-    });
-    Route::get('/two', function() {
-        return "This is second Group Middleware";
-    });
-});
-
-// Basic Controllers
-Route::get('basic-controller', [BasicController::class, 'index']);
-
-// Data Retrival
-Route::post('/dataretrival', [DataRetrival::class, 'store']);
-Route::get('/form', function () {
-    return view('DataRetrival');
-});
-
-// Cookie
-Route::get('/set-cookie', function(){
-    return response("Cookie Set")->cookie('user', 'Amit', 60);
-});
-Route::get('/get-cookie', function(Request $request){
-    return $request->cookie('user');
-    // return "Cookie value: " . $request->cookie('user');
-});
-Route::get('/delete-cookie', function(){
-    return response("Cookie Deleted")->cookie('user', null, -1);
-});
-
-// Session
-Route::get('/set-session', function (Request $request) {
-    $request->session()->put('user', 'Amit');
-    return "Session Stored";
-});
-Route::get('/get-session', function (Request $request) {
-    return $request->session()->get('user');
-});
-Route::get('/delete-session', function (Request $request) {
-    $request->session()->forget('user');
-    return "Session Deleted";
-});
-
-// Localisation
-Route::get('/local', function (){
-    return view('Localization');
-});
-
-
-// Email Sending Route
-Route::get('/send-mail', [SendEmail::class, 'send']);
-
-// File Uploading Route
-Route::get('/upload', function () {
-    return view('SendFile');
-});
-Route::post('/upload', [SendFileController::class, 'upload']);
-
-// Form Validation Route
-Route::get('/show-form', [StudentFormController::class, 'showForm']);
-Route::post('/submit-form', [StudentFormController::class, 'submitForm']);
-
-
-// Route::get('/insert', [StudentController::class, 'insert']);
-// Route::get('/read', [StudentController::class, 'read']);
-// Route::get('/update/{id}', [StudentController::class, 'update']);
-// Route::get('/delete/{id}', [StudentController::class, 'delete']);
-
-// API
-Route::get('/get-quotes',[APIController::class, 'fetch']);
-
-// Database Connection Check
-Route::get('/check-db', function() {
-    DB::connection()->getMongoClient();
-
-    return "MongoDB Connected Successfully";
-});
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Route::get('/home', function() {
-//     return view('home', ['name' => 'Aman']);
+// // Group Middleware
+// Route::middleware('ageGroup')->group(function() {
+//     Route::get('/one', function() {
+//         return "This is first Group Middleware";
+//     });
+//     Route::get('/two', function() {
+//         return "This is second Group Middleware";
+//     });
 // });
 
-use App\Http\Controllers\UserController;
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}',[UserController::class, 'show']);
+// // Basic Controllers
+// Route::get('basic-controller', [BasicController::class, 'index']);
+
+// // Data Retrival
+// Route::post('/dataretrival', [DataRetrival::class, 'store']);
+// Route::get('/form', function () {
+//     return view('DataRetrival');
+// });
+
+// // Cookie
+// Route::get('/set-cookie', function(){
+//     return response("Cookie Set")->cookie('user', 'Amit', 60);
+// });
+// Route::get('/get-cookie', function(Request $request){
+//     return $request->cookie('user');
+//     // return "Cookie value: " . $request->cookie('user');
+// });
+// Route::get('/delete-cookie', function(){
+//     return response("Cookie Deleted")->cookie('user', null, -1);
+// });
+
+// // return response("Cookie Set")->cookie('user', 'Amit', 60);
+// // return $request->cookie('user');
+// // return response("Cookie Deleted")->cookie('user', null, -1);
 
 
-// Types of Routing 
-
-// Named Route
-Route::get('/home', function(){
-    return view('home');
-});
-
-Route::get('/about1/about2/about3', function() {
-    return view('about');
-});
-
-Route::get('/contact1/contact2/contact3', function() {
-    return view('contact');
-})->name('cont');
+// // $request->session()->put('user', 'Amit');
+// // return "Session Stored";
+// // return $request->session()->get('user');
+// // $request->session()->forget('user');
+// // return "Session Deleted";
 
 
-//Route Prefixing
-Route::prefix('admin')->group(function () {
+// // Session
+// Route::get('/set-session', function (Request $request) {
+//     $request->session()->put('user', 'Amit');
+//     return "Session Stored";
+// });
+// Route::get('/get-session', function (Request $request) {
+//     return $request->session()->get('user');
+// });
+// Route::get('/delete-session', function (Request $request) {
+//     $request->session()->forget('user');
+//     return "Session Deleted";
+// });
 
-    Route::get('/dashboard', function () {
-        return "Admin Dashboard";
-    });
+// // Localisation
+// Route::get('/local', function (){
+//     return view('Localization');
+// });
 
-    Route::get('/users', function () {
-        return "Admin Users";
-    });
 
-});
+// // Email Sending Route
+// Route::get('/send-mail', [SendEmail::class, 'send']);
 
-//Domain Routing
-Route::domain('admin.localhost')->group(function () { 
-    // http://admin.localhost:8000 --> for checking
-    Route::get('/ab', function () {
-        return "Admin Panel";
-    });
-});
+// // File Uploading Route
+// Route::get('/upload', function () {
+//     return view('SendFile');
+// });
+// Route::post('/upload', [SendFileController::class, 'upload']);
 
-// Open
-// C:\Windows\System32\drivers\etc\hosts
-// Add this line:
-// 127.0.0.1 admin.localhost
+// // Form Validation Route
+// Route::get('/show-form', [StudentFormController::class, 'showForm']);
+// Route::post('/submit-form', [StudentFormController::class, 'submitForm']);
 
-// to get current URL
-use Illuminate\Support\Facades\URL;
 
-Route::get('/test1', function () {
-    return URL::current();
-});
+// // Route::get('/insert', [StudentController::class, 'insert']);
+// // Route::get('/read', [StudentController::class, 'read']);
+// // Route::get('/update/{id}', [StudentController::class, 'update']);
+// // Route::get('/delete/{id}', [StudentController::class, 'delete']);
 
-Route::get('/component', function () {
-    return view('component');
-})->name('component');
+// // API
+// Route::get('/get-quotes',[APIController::class, 'fetch']);
 
-Route::get('/test2', function () {
-    return route('component');
-});
+// // Database Connection Check
+// Route::get('/check-db', function() {
+//     DB::connection()->getMongoClient();
 
-Route::get('/user/{id}', function ($id) {
-    return "User ID: " . $id;
-});
+//     return "MongoDB Connected Successfully";
+// });
 
-Route::get('/test3', function () {
-    return url('/home');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-// Asset URL
-Route::get('/asset', function() {
-    return view('AssetUrl');
-});
+// // Route::get('/home', function() {
+// //     return view('home', ['name' => 'Aman']);
+// // });
 
-// Action
-use App\Http\Controllers\ActionController;
-Route::get('/action', [ActionController::class, 'index']);
+// use App\Http\Controllers\UserController;
+// Route::get('/users', [UserController::class, 'index']);
+// Route::get('/users/{id}',[UserController::class, 'show']);
 
-// Passing Parameter to the route
-Route::get('/post/{id}/{name}', function($id, $name) {
-    return $id . " " . $name;
-});
 
-// Passing views to the route
-Route::get('/dashboard', function() {
-    return view('Dashboard');
-});
+// // Types of Routing 
 
-// Passing Data to Views
+// // Named Route
+// Route::get('/home', function(){
+//     return view('home');
+// });
 
-Route::get('/profile', function() {
-    // Using with()
-    // return view('Profile')->with('name', 'Rahul');
+// Route::get('/about1/about2/about3', function() {
+//     return view('about');
+// });
 
-    // Using Array
-    // return view('profile', ['name' => 'Rahul', 'age' => 20]);
+// Route::get('/contact1/contact2/contact3', function() {
+//     return view('contact');
+// })->name('cont');
 
-    // Using compact()
-    $name = "Rahul";
-    $age = 20;
-    return view('profile', compact('name','age'));
 
-});
+// //Route Prefixing
+// Route::prefix('admin')->group(function () {
 
-// Attaching Headers
-Route::get('/header', function() {
-    return response('Hello')
-    ->header('Content-Type', 'text/plain');
-});
+//     Route::get('/dashboard', function () {
+//         return "Admin Dashboard";
+//     });
 
-// Attaching Cookies
-Route::get('/cookie1', function() {
-    return response("Cookie Set") 
-    ->cookie('name', 'Rahul', 60);
-});
+//     Route::get('/users', function () {
+//         return "Admin Users";
+//     });
 
-// JSON Response
-Route::get('/json', function() {
-    return response()->json(['name' => 'Rahul', 'age' => 20]);
-});
+// });
 
-// Laravel Redirections
-Route::get('/redirect', function () {
-    return redirect('home');
-});
+// //Domain Routing
+// Route::domain('admin.localhost')->group(function () { 
+//     // http://admin.localhost:8000 --> for checking
+//     Route::get('/ab', function () {
+//         return "Admin Panel";
+//     });
+// });
 
-// Redirecting to Named Routes
-Route::get('/chat', function() {
-    return "Chat Page";
-})->name('chat');
-Route::get('go-chat', function() {
-    return redirect()->route('chat');
-});
+// // Open
+// // C:\Windows\System32\drivers\etc\hosts
+// // Add this line:
+// // 127.0.0.1 admin.localhost
 
-// Redirecting to Controller Actions
-use App\Http\Controllers\RedirectControllerAction;
-Route::get('/con', [RedirectControllerAction::class, 'index']);
-Route::get('/go-con', function() {
-    return redirect()->action([RedirectControllerAction::class, 'index']);
-});
+// // to get current URL
+// use Illuminate\Support\Facades\URL;
+
+// Route::get('/test1', function () {
+//     return URL::current();
+// });
+
+// Route::get('/component', function () {
+//     return view('component');
+// })->name('component');
+
+// Route::get('/test2', function () {
+//     return route('component');
+// });
+
+// Route::get('/user/{id}', function ($id) {
+//     return "User ID: " . $id;
+// });
+
+// Route::get('/test3', function () {
+//     return url('/home');
+// });
+
+// // Asset URL
+// Route::get('/asset', function() {
+//     return view('AssetUrl');
+// });
+
+// // Action
+// use App\Http\Controllers\ActionController;
+// Route::get('/action', [ActionController::class, 'index']);
+
+// // Passing Parameter to the route
+// Route::get('/post/{id}/{name}', function($id, $name) {
+//     return $id . " " . $name;
+// });
+
+// // Passing views to the route
+// Route::get('/dashboard', function() {
+//     return view('Dashboard');
+// });
+
+// // Passing Data to Views
+
+// Route::get('/profile', function() {
+//     // Using with()
+//     // return view('Profile')->with('name', 'Rahul');
+
+//     // Using Array
+//     // return view('profile', ['name' => 'Rahul', 'age' => 20]);
+
+//     // Using compact()
+//     $name = "Rahul";
+//     $age = 20;
+//     return view('profile', compact('name','age'));
+
+// });
+
+// // Attaching Headers
+// Route::get('/header', function() {
+//     return response('Hello')
+//     ->header('Content-Type', 'text/plain');
+// });
+
+// // Attaching Cookies
+// Route::get('/cookie1', function() {
+//     return response("Cookie Set") 
+//     ->cookie('name', 'Rahul', 60);
+// });
+
+// // JSON Response
+// Route::get('/json', function() {
+//     return response()->json(['name' => 'Rahul', 'age' => 20]);
+// });
+
+// // Laravel Redirections
+// Route::get('/redirect', function () {
+//     return redirect('home');
+// });
+
+// // Redirecting to Named Routes
+// Route::get('/chat', function() {
+//     return "Chat Page";
+// })->name('chat');
+// Route::get('go-chat', function() {
+//     return redirect()->route('chat');
+// });
+
+// // Redirecting to Controller Actions
+// use App\Http\Controllers\RedirectControllerAction;
+// Route::get('/con', [RedirectControllerAction::class, 'index']);
+// Route::get('/go-con', function() {
+//     return redirect()->action([RedirectControllerAction::class, 'index']);
+// });
+
+
+
